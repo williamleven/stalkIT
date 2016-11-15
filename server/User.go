b545d1799fd8b5635956586820655af194f1aa4d
Gurgy <gurgy@emycia.com>
2016-11-15 20:19:15 +0100

    Added StartTime to user struct for future use

--- server/User.go
+++ server/User.go
@@ -1,9 +1,12 @@
 package main
 
+import "time"
+
 // Holds smurf data
 type User struct {
-	Id   string `json:"user_id"`
-	Nick string `json:"nick"`
+	Id   		string		`json:"user_id"`
+	Nick 		string 		`json:"nick"`
+	StartTime	time.Time	`json:"start_time"`
 }
 
 type Users []User
