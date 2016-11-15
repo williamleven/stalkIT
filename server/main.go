4bf32126712fc516f53e0164fd7fb15227022c7f
Gurgy <gurgy@emycia.com>
2016-11-15 15:26:45 +0100

    multiple connections

--- server/main.go
+++ server/main.go
@@ -1,21 +1,13 @@
 package main
 
-import "net"
 
 func main() {
 	input := make(chan *Users) 		// Delivering user Lists
 	outputArrivals := make(chan *User) 	// Delivering users arriving
 	outputDepartures := make(chan *User) 	// Delivering users leaving
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":7825")
-
-	// accept connection on port
-	conn, _ := ln.Accept()
-
 	// Handles frontend notifications
-	go notifier(outputArrivals, true, conn)
-	go notifier(outputDepartures, false, conn)
+	go notifier(outputArrivals, outputDepartures)
 
 	// Coordinates data-flow
 	go coordinate(input, outputArrivals, outputDepartures)
--- server/notifier.go
+++ server/notifier.go
@@ -5,12 +5,46 @@ import (
 )
 
 // Sends notification in the format input + append
-func notifier(input chan *User, arriving bool, conn net.Conn)  {
+func notifier(arrivals chan *User, departures chan *User)  {
+
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", ":7825")
+
+	// accept connection on port
+	//conn, _ := ln.Accept()
+
+	connections := make(map[net.Conn]bool)
+
+	go func(clients *map[net.Conn]bool) {
+		for {
+			conn, _ := ln.Accept()
+			(*clients)[conn] = true
+		}
+	}(&connections)
+
+
+	c := make(chan string)
+	go func() {
+		for {
+			user := <-arrivals
+			c <- "A"+user.Nick
+		}
+	}()
+	go func() {
+		for {
+			user := <-departures
+			c <- "D"+user.Nick
+		}
+	}()
 
 	// Pushing notifications whenever receiving input
 	for {
-		user := <-input
-
-		conn.Write([]byte(int8(arriving) + user.Nick + "\n"))
+		message := <-c
+		for conn, value := range connections {
+			if value {
+				conn.Write([]byte(message + "\n"))
+			}
+		}
 	}
 }
+
